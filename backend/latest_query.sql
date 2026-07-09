687575652221fcfe9c89fb20436882b5b687388b
prowizt <prowiz@icloud.com>
2026-07-09 14:02:39 +0900

    HTML스크랩개선직전백업

--- backend/latest_query.sql
+++ backend/latest_query.sql
@@ -1,3 +1,6 @@
-SELECT DISTINCT [지원학과명]
-  FROM [UI_IPSI_M_V] 
-  WHERE [지원학과명] LIKE '%소믈리에%' OR [지원학과명] LIKE '%디저트%'
+SELECT [모집학과명],
+         [정원내외구분],
+         [전형명],
+         [모집인원]
+  FROM [EXCEL_ADMISSIONQUOTA] 
+  WHERE [학년도] = '2027' AND [모집구분] LIKE '%수시1차%'
